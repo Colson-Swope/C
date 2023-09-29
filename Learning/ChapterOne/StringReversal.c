e3d6357cc1481551886e8eb2d59838b7f61a0447
Colson-Swope <colsonjswope@gmail.com>
2023-09-29 10:53:27 -0700

    Class-PC 092923 1053

---
+++ Learning/ChapterOne/StringReversal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char reverse(char s[]);
+
+int main() 
+{
+    char s[11] = "teststring";
+    printf("%d\n", reverse(s));
+
+}
+
+char reverse(char s[]) 
+{
+    // LEFT OFF HERE 
+    return 'n';
+}
