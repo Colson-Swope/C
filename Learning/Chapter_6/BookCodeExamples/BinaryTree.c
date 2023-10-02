e8a48bd491ac67ca007db5b209d183a95adef79c
Colson-Swope <colsonjswope@gmail.com>
2023-10-02 11:54:44 -0700

    Lab-PC 100223 1154

---
+++ Learning/Chapter_6/BookCodeExamples/BinaryTree.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXWORD 100
+struct tnode *addtree(struct tnode *, char *);
+struct tnode *talloc(void);
+
+char *strdup(char *);
+
+void treeprint(struct tnode *);
+int getword(char *, int);
+
+struct tnode {
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+int main() 
+{
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+
+    while (getword(word, MAXWORD) != EOF) {
+        if (isalpha(word[0])) {
+            root = addtree(root, word);
+        }
+    }
+    treeprint(root);
+    return 0;
+}
+
+int getword(char *word, int lim) 
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+
+    while (isspace(c = getch())) {
+        ;
+    }
+    if (c != EOF) {
+        *w++ = c;
+    }
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++) {
+        if (!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
+}
+
+void treeprint(struct tnode *p) 
+{
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+int tree
+{
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *strdub(char *s) // left off on page 142 
--- Learning/Chapter_6/BookCodeExamples/CWordOccurrences.c
+++ Learning/Chapter_6/BookCodeExamples/CWordOccurrences.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+struct key {
+    char *word;
+    int count;
+}; // keytab[];
+
+// see above for alt way of declare 
+struct key keytab[] = {
+    "auto", 0,
+    "break", 0, 
+    "case", 0,
+    "char", 0,
+    "const", 0,
+    "continue", 0,
+    "default", 0,
+    /* ... */
+    "unsigned", 0,
+    "void", 0,
+    "volatile", 0,
+    "while", 0
+};
+
 int main() 
 {
-    
+
     return 0;
 }
