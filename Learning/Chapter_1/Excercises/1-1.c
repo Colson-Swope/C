661a53563f3758c64c32966ef30f84010936377c
Colson-Swope <colsonjswope@gmail.com>
2023-12-18 14:08:20 -0800

    moved all scripts into repo

--- Learning/Chapter_1/Excercises/1-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// TASK: Write a hello world program 
-
-int main() 
-{
-    printf("Hello World\n");
-    return 0;
-}
--- Learning/Chapter_2/DataTypeSizes.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <limits.h>
-#include <float.h>
-
-int main() 
-{
-    printf("char length in bits: %d\n", CHAR_BIT);
-    printf("int length in bits: %d\n", )
-}
